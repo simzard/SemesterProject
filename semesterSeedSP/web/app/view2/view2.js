d260c2f1f1cb67b7ef61446f7f07a5d2b75f2c92
Simon Steinaa <thatguy@simonsteinaa.dk>
2015-12-16 15:41:48 +0100

    fixed time on destination date on tickets view2

--- semesterSeedSP/web/app/view2/view2.js
+++ semesterSeedSP/web/app/view2/view2.js
@@ -12,8 +12,30 @@ angular.module('myApp.view2', ['ngRoute'])
 
         .controller('View2Ctrl', ["AirlineFactory", function (AirlineFactory) {
             var self = this;
+    
+            self.calcEndDate = function (startDate, travelTime) {
+                var hours = Math.floor(parseInt(travelTime) / 60);
+                var minutes = parseInt(travelTime) % 60;
+                console.log("hours: " + hours + " minutes:" + minutes)
+                
+                var tmpDate = new Date(startDate);
+                
+                var hourSum = tmpDate.getHours() + hours;
+                var minuteSum = tmpDate.getMinutes() + minutes;
+                
+                var resultISOdate = new Date(
+                                    tmpDate.getFullYear(),
+                                    tmpDate.getMonth(),
+                                    tmpDate.getDate(),
+                                    hourSum,
+                                    minuteSum).toISOString();
+                                            
+             
+                //console.log("resultISOdate:" + resultISOdate);
+                return resultISOdate;
+            }
             
             self.flightsInfo = AirlineFactory.getFlights();
-            console.log(self.flightsInfo);
+            //console.log(self.flightsInfo);
 
         }]);
